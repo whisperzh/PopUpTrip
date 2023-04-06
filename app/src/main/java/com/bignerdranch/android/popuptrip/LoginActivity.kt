63e766ca7c11135aa86375d89de4905fcc68877b
Renming Zhang <rmzhang@bu.edu>
2023-04-06 13:17:39 -0400

    1. cleaned unused code

--- app/src/main/java/com/bignerdranch/android/popuptrip/LoginActivity.kt
+++ app/src/main/java/com/bignerdranch/android/popuptrip/LoginActivity.kt
@@ -73,21 +73,6 @@ class LoginActivity : AppCompatActivity() {
 // system permissions dialog. Save the return value, an instance of
 // ActivityResultLauncher. You can use either a val, as shown in this snippet,
 // or a lateinit var in your onAttach() or onCreate() method.
-//        val requestPermissionLauncher =
-//            registerForActivityResult(
-//                ActivityResultContracts.RequestPermission()
-//            ) { isGranted: Boolean ->
-//                if (isGranted) {
-//                    // Permission is granted. Continue the action or workflow in your
-//                    // app.
-//                } else {
-//                    // Explain to the user that the feature is unavailable because the
-//                    // feature requires a permission that the user has denied. At the
-//                    // same time, respect the user's decision. Don't link to system
-//                    // settings in an effort to convince the user to change their
-//                    // decision.
-//                }
-//            }
 
         val requestPermissionLauncher =
             registerForActivityResult(
