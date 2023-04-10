8227d4ae31628958c82f091c8e627ad5ecf9f253
Melody Chan <melodyy@bu.edu>
2023-04-10 12:39:20 -0400

    attempt for ExplorationMapFragment

--- app/src/main/java/com/bignerdranch/android/popuptrip/ui/home/ExplorationMapFragment.kt
+++ app/src/main/java/com/bignerdranch/android/popuptrip/ui/home/ExplorationMapFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bignerdranch.android.popuptrip.R
-import com.bignerdranch.android.popuptrip.databinding.FragmentMapBinding
+import com.bignerdranch.android.popuptrip.databinding.FragmentExplorationMapBinding
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,8 +16,8 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 private const val TAG = "HomeMapFragment"
 
-class HomeMapFragment: Fragment() {
-    private var _binding: FragmentMapBinding? = null
+class ExplorationMapFragment: Fragment(), OnMapReadyCallback {
+    private var _binding: FragmentExplorationMapBinding? = null
     private lateinit var mMap: GoogleMap
 
     private val binding
@@ -25,25 +25,27 @@ class HomeMapFragment: Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
-    override fun onCreate(savedInstanceState: Bundle?){
-        super.onCreate(savedInstanceState)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment =
-        mapFragment.getMapAsync(this)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentMapBinding.inflate(inflater, container, false)
-
+        _binding = FragmentExplorationMapBinding.inflate(inflater, container, false)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
