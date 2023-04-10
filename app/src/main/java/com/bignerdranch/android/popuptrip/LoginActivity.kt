e09d6d66ea0086f17f87883fba379492f1af1965
Alexstarwing <441698225@qq.com>
2023-04-10 19:20:04 -0400

    google sign in navigation dialog

--- app/src/main/java/com/bignerdranch/android/popuptrip/LoginActivity.kt
+++ app/src/main/java/com/bignerdranch/android/popuptrip/LoginActivity.kt
@@ -2,13 +2,17 @@ package com.bignerdranch.android.popuptrip
 
 import android.Manifest
 import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
 import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.bignerdranch.android.popuptrip.databinding.ActivityLoginBinding
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
@@ -47,6 +51,7 @@ class LoginActivity : AppCompatActivity() {
             .setAutoSelectEnabled(true)
             .build()
         binding.signInWithGoogleButton.setOnClickListener {
+            //TODO:
             oneTapClient.beginSignIn(signInRequest)
                 .addOnSuccessListener(this) { result ->
                     try {
@@ -60,33 +65,45 @@ class LoginActivity : AppCompatActivity() {
                 .addOnFailureListener(this) { e ->
                     // No saved credentials found. Launch the One Tap sign-up flow, or
                     // do nothing and continue presenting the signed-out UI.
+                    //TODO:login failed
+                    val alertDialog = AlertDialog.Builder(this)
+                        .setTitle("Login Failed")
+                        .setMessage("The login failed. Please check your credentials and try again.")
+                        .setPositiveButton("OK") { _, _ ->
+                        }
+                        .create()
+
+                    alertDialog.show()
                     Log.d(TAG, e.localizedMessage)
                 }
         }
 
-
-
-
-
-
         // Register the permissions callback, which handles the user's response to the
-// system permissions dialog. Save the return value, an instance of
-// ActivityResultLauncher. You can use either a val, as shown in this snippet,
-// or a lateinit var in your onAttach() or onCreate() method.
-
+        // system permissions dialog. Save the return value, an instance of
+        // ActivityResultLauncher. You can use either a val, as shown in this snippet,
+        // or a lateinit var in your onAttach() or onCreate() method.
         val requestPermissionLauncher =
             registerForActivityResult(
                 ActivityResultContracts.RequestPermission()
             ) { isGranted: Boolean ->
                 if (isGranted) {
-                    // Permission is granted. Continue the action or workflow in your
-                    // app.
+                    // Permission is granted. Continue the action or workflow in your app.
                 } else {
                     // Explain to the user that the feature is unavailable because the
                     // feature requires a permission that the user has denied. At the
                     // same time, respect the user's decision. Don't link to system
                     // settings in an effort to convince the user to change their
                     // decision.
+                    //Toast.makeText(this,"Sorry, you cannot login without permission",Toast.LENGTH_LONG).show()
+                    //Write dialog
+                    val alertDialog = AlertDialog.Builder(this)
+                    alertDialog.setTitle("Permission denied")
+                        .setMessage("Sorry, you cannot login without permission.")
+                        .setPositiveButton("OK") { _, _ ->
+                        }
+                        .create()
+
+                    alertDialog.show()
                 }
             }
 
@@ -107,13 +124,26 @@ class LoginActivity : AppCompatActivity() {
             -> {
                 // You can use the API that requires the permission.
             }
-            shouldShowRequestPermissionRationale(Manifest.permission.INTERNET) -> {
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_NETWORK_STATE) -> {
             // In an educational UI, explain to the user why your app requires this
             // permission for a specific feature to behave as expected, and what
             // features are disabled if it's declined. In this UI, include a
             // "cancel" or "no thanks" button that lets the user continue
             // using your app without granting the permission.
 //            showInContextUI(...)
+                val alertDialog = AlertDialog.Builder(this)
+                    .setTitle("Network State Permission Required")
+                    .setMessage("This app requires access to the network state to provide a better user experience. " +
+                            "Granting this permission allows the app to check if your device is connected to the internet before login.")
+                    .setPositiveButton("OK") { _, _ ->
+                        requestPermissionLauncher.launch(Manifest.permission.ACCESS_NETWORK_STATE)
+                    }
+                    .setNegativeButton("No Thanks") { _, _ ->
+                        //TODO:user don't give permission
+                    }
+                    .create()
+
+                alertDialog.show()
         }
             else -> {
                 // You can directly ask for the permission.
