d3c86644f102e3885162d8611a0544fbe6fe703b
Sarah Bonna <sbonna@bu.edu>
2023-04-11 09:37:21 -0400

    Attempted navigation to exploration page. Not working yet.

--- app/src/main/java/com/bignerdranch/android/popuptrip/ui/home/ExplorationFragment.kt
+++ app/src/main/java/com/bignerdranch/android/popuptrip/ui/home/ExplorationFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.bignerdranch.android.popuptrip.R
+import com.bignerdranch.android.popuptrip.databinding.FragmentExplorationBinding
 import com.bignerdranch.android.popuptrip.databinding.FragmentExplorationMapBinding
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -15,11 +18,12 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.textfield.TextInputLayout
 
 private const val TAG = "ExplorationMapFragment"
 
-class ExplorationMapFragment: Fragment(), OnMapReadyCallback {
-    private var _binding: FragmentExplorationMapBinding? = null
+class ExplorationFragment: Fragment(), OnMapReadyCallback {
+    private var _binding: FragmentExplorationBinding? = null
     private lateinit var mMap: GoogleMap
 
     private val binding
@@ -27,15 +31,17 @@ class ExplorationMapFragment: Fragment(), OnMapReadyCallback {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
-    private val args: ExplorationMapFragmentArgs by navArgs()
+    private val explorationViewModel: ExplorationViewModel by viewModels()
+//    private val args: ExplorationFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentExplorationMapBinding.inflate(inflater, container, false)
-        Log.d(TAG, args.destination)
+        _binding = FragmentExplorationBinding.inflate(inflater, container, false)
+//        Log.d(TAG, args.destination)
+//        val fragment = childFragmentManager.findFragmentById(R.id.explorationSearchboxFragmentContainerView)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         return binding.root
