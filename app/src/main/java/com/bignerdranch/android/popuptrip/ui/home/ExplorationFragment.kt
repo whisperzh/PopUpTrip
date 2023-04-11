3c56a65d52f2c45bc61f0a33f40a1bf948665b70
Melody Chan <melodyy@bu.edu>
2023-04-11 17:06:20 -0400

    fixed exploration page, exploration page is now under dashboard tab

--- app/src/main/java/com/bignerdranch/android/popuptrip/ui/home/ExplorationFragment.kt
+++ app/src/main/java/com/bignerdranch/android/popuptrip/ui/home/ExplorationFragment.kt
@@ -1,62 +1,35 @@
 package com.bignerdranch.android.popuptrip.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentContainerView
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
-import com.bignerdranch.android.popuptrip.R
+import androidx.lifecycle.ViewModelProvider
 import com.bignerdranch.android.popuptrip.databinding.FragmentExplorationBinding
-import com.bignerdranch.android.popuptrip.databinding.FragmentExplorationMapBinding
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.material.textfield.TextInputLayout
+import com.bignerdranch.android.popuptrip.databinding.FragmentHomeBinding
 
-private const val TAG = "ExplorationMapFragment"
+class ExplorationFragment: Fragment() {
 
-class ExplorationFragment: Fragment(), OnMapReadyCallback {
     private var _binding: FragmentExplorationBinding? = null
-    private lateinit var mMap: GoogleMap
 
-    private val binding
-        get() = checkNotNull(_binding) {
-            "Cannot access binding because it is null. Is the view visible?"
-        }
-
-    private val explorationViewModel: ExplorationViewModel by viewModels()
-//    private val args: ExplorationFragmentArgs by navArgs()
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
         _binding = FragmentExplorationBinding.inflate(inflater, container, false)
-//        Log.d(TAG, args.destination)
-//        val fragment = childFragmentManager.findFragmentById(R.id.explorationSearchboxFragmentContainerView)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
