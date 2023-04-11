1a4bc81bef356e392c364c94c3ced09b33560f7a
Renming Zhang <rmzhang@bu.edu>
2023-04-10 21:29:31 -0400

    try connect two activities

--- app/src/main/java/com/bignerdranch/android/popuptrip/LoginActivity.kt
+++ app/src/main/java/com/bignerdranch/android/popuptrip/LoginActivity.kt
@@ -30,6 +30,11 @@ class LoginActivity : AppCompatActivity() {
 
         binding= ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.LoginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            // start your next activity
+            startActivity(intent)
+        }
         oneTapClient = Identity.getSignInClient(this)
         signInRequest = BeginSignInRequest.builder()
             .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
