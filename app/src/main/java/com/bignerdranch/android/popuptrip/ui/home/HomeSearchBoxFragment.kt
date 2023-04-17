9f7e9cd4f61d796ddab747b82894468c1af01da6
Melody Chan <melodyy@bu.edu>
2023-04-17 17:30:27 -0400

    home searchbox, added Google autocomplete widget

--- app/src/main/java/com/bignerdranch/android/popuptrip/ui/home/HomeSearchBoxFragment.kt
+++ app/src/main/java/com/bignerdranch/android/popuptrip/ui/home/HomeSearchBoxFragment.kt
@@ -6,7 +6,14 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import com.bignerdranch.android.popuptrip.databinding.FragmentHomeSearchBoxBinding
 import androidx.fragment.app.viewModels
+import com.bignerdranch.android.popuptrip.BuildConfig.MAPS_API_KEY
 import com.bignerdranch.android.popuptrip.R
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import java.util.*
 
 private const val TAG = "HomeSearchBoxFragment"
 class HomeSearchBoxFragment: Fragment() {
@@ -21,6 +28,10 @@ class HomeSearchBoxFragment: Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!Places.isInitialized()) {
+            Places.initialize(requireContext(), MAPS_API_KEY, Locale.US);
+        }
         Log.d(TAG, "onCraete has been called")
 //        setHasOptionsMenu(true)
     }
@@ -31,14 +42,32 @@ class HomeSearchBoxFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeSearchBoxBinding.inflate(inflater, container, false)
-        val destEntered = homeSearchBoxViewModel.dest
-        binding.homeSearchBar.setText(destEntered)
+//        val destEntered = homeSearchBoxViewModel.dest
+//        binding.homeSearchBar.setText(destEntered)
 
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val autocompleteFragment = childFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                as AutocompleteSupportFragment
+
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+            }
+
+            override fun onError(status: Status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: $status")
+            }
+        })
     }
 
     override fun onDestroyView() {
