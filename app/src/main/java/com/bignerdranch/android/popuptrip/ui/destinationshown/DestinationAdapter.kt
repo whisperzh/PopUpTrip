5ffe1801e6802386758e495e71bdbc9cbcf8c025
yuhe bian <yuheb@bu.edu>
2023-05-09 00:14:19 -0400

    http request for itinerary detailed view finished

--- app/src/main/java/com/bignerdranch/android/popuptrip/ui/destinationshown/DestinationAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/popuptrip/ui/destinationshown/DestinationAdapter.kt
@@ -7,38 +7,46 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.popuptrip.R
 
-data class ItineraryItem(val destination: String, val timeToNext: String)
+// Define a data class for ItineraryItem with destination and timeToNext properties
+data class DestinationItem(val destination: String, val timeToNext: String)
 
-class ItineraryAdapter(private val destinations: List<ItineraryItem>) :
+// Define the ItineraryAdapter class which extends RecyclerView.Adapter with a ViewHolder
+class ItineraryAdapter(private val destinations: MutableList<DestinationItem>) :
     RecyclerView.Adapter<ItineraryAdapter.ViewHolder>() {
 
+    // Define a ViewHolder class for the ItineraryAdapter, responsible for holding view references
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val destinationNameTextView: TextView = itemView.findViewById(R.id.tv_destination_name)
         val timeToNextTextView: TextView = itemView.findViewById(R.id.tv_time_to_next)
         val arrowTextView: TextView = itemView.findViewById(R.id.tv_arrow)
     }
 
-
+    // Inflate the layout for each item in the list and create a ViewHolder with the inflated view
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.destination_item, parent, false)
         return ViewHolder(itemView)
     }
 
+    // Bind the data from the current ItineraryItem to the ViewHolder's views
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        if (position < destinations.size) {
-            val currentItem = destinations[position]
-            holder.destinationNameTextView.text = currentItem.destination
+        val currentItem = destinations[position]
+        holder.destinationNameTextView.text = currentItem.destination
+
+        if (position < destinations.size-1) {
             holder.timeToNextTextView.text = currentItem.timeToNext
             holder.arrowTextView.visibility = View.VISIBLE
         } else {
-            holder.destinationNameTextView.text = "Final Destination"
-            holder.timeToNextTextView.text = "N/A"
+            holder.timeToNextTextView.text = ""
             holder.arrowTextView.visibility = View.GONE
         }
     }
+    // Return the total number of items, including an extra item for the final destination
+    override fun getItemCount(): Int = destinations.size
 
-
-    override fun getItemCount(): Int = destinations.size + 1
-
+    // Add a new ItineraryItem to the list and update the adapter
+    fun addDestination(destination: DestinationItem) {
+        destinations.add(destination)
+        notifyDataSetChanged()
+    }
 }
---
+++ app/src/main/java/com/bignerdranch/android/popuptrip/ui/destinationshown/ItineraryDetailedFragment.kt
@@ -0,0 +1,130 @@
+package com.bignerdranch.android.popuptrip.ui.itinerary
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.bignerdranch.android.popuptrip.MainActivity
+import com.bignerdranch.android.popuptrip.R
+import com.bignerdranch.android.popuptrip.databinding.FragmentItineraryBinding
+import com.bignerdranch.android.popuptrip.ui.home.HomeFragmentDirections
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+import org.json.JSONArray
+
+
+class ItineraryDetailedFragment : Fragment() {
+    private val TAG = "class ItineraryFragment : Fragment()"
+    private lateinit var itineraryAdapter: ItineraryAdapter
+    private val args: ItineraryDetailedFragmentArgs by navArgs()
+    private var dbReference: DatabaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://popup-trip-default-rtdb.firebaseio.com/")
+
+    private lateinit var binding: FragmentItineraryBinding
+
+    // Declare a URL for getting itinerary data
+    private var get_one_itinerary_url: String = "http://54.147.60.104/itinerary/get-one-itinerary/"
+//"http://54.147.60.104/itinerary/get-one-itinerary/[itinerary_id]/[travel_preference]"
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        binding=FragmentItineraryBinding.inflate(layoutInflater,container,false)
+
+        // Get a reference to the RecyclerView
+        val recyclerView = binding.itineraryRecyclerView
+        binding.itDetailBackButton.setOnClickListener {
+//            viewLifecycleOwner.lifecycleScope.launch {
+//                viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+//                    findNavController().navigate(
+//                        ItineraryDetailedFragmentDirections.actionItineraryFragmentToNavigationDashboard()
+//                    )
+//                }
+//            }
+            activity!!.onBackPressed()
+        }
+
+        // Initialize a list of ItineraryItems with some example data
+        val destinations = mutableListOf<DestinationItem>()
+
+        // Initialize the ItineraryAdapter with the list of ItineraryItems
+        itineraryAdapter = ItineraryAdapter(destinations)
+        // Set the RecyclerView's adapter to the ItineraryAdapter
+        recyclerView.adapter = itineraryAdapter
+        // Set the RecyclerView's layout manager to a LinearLayoutManager
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // Check if there are arguments passed to this fragment
+        if (args != null) {
+            // Log the itineraryId argument
+            Log.d(TAG, args.itineraryId)
+
+            var travelM=(activity as MainActivity).user.travelMethod.toLowerCase()
+
+
+            var userEmail:String = Firebase.auth.currentUser!!.email!!
+            var wholeUrl:String=get_one_itinerary_url+args.itineraryId.toString()+"/"+travelM
+            getVolley(wholeUrl)
+
+        } else {
+            // Log that there are no arguments
+            Log.d(TAG, "NO args")
+        }
+
+        return  binding.root
+    }
+
+    // Define a function to add a new destination to the list and update the adapter
+        private fun addDestination(destinationName: String, timeToNext: String) {
+        // Create a new ItineraryItem with the given destinationName and timeToNext
+        val newDestination = DestinationItem(destinationName, timeToNext)
+        // Add the new ItineraryItem to the adapter's list
+        itineraryAdapter.addDestination(newDestination)
+    }
+
+    private fun getVolley(url:String){
+        // Instantiate the RequestQueue.
+        val queue = Volley.newRequestQueue(context)
+        // Request a string response from the provided URL.
+        val stringReq = JsonObjectRequest(
+            Request.Method.GET, url,
+            null,{ response ->
+                if(response.get("status code").toString().equals("200"))
+                {
+                    var k=response.getJSONArray("route")
+
+//                    var listOfItinerarys:MutableList<Itinerary> = mutableListOf()
+                    for(i in 0 until k.length())
+                    {
+                        var singlePosition=k.get(i) as JSONArray
+                        addDestination(singlePosition.get(0).toString(),singlePosition.get(1).toString())
+                    }
+//                    dashboardViewModel.setFlow(listOfItinerarys)
+                }
+                else
+                {
+                    Toast.makeText(activity, getString(R.string.itinerary_error), Toast.LENGTH_LONG).show()
+                }
+                Log.d("Uni-Api","Sam succeeded in providing data")
+            },
+            Response.ErrorListener {Log.d("API", "that didn't work") })
+        queue.add(stringReq)
+    }
+
+}
+
